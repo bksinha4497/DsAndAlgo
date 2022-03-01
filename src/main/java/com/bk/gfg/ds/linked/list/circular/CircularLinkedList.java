7151cd0cc9a976db6acdefbf654891a778bd0c58
biswajit.kumar1 <biswajit.kumar1@comviva.com>
2022-03-01 13:56:43 +0530

    Circular Linked List Start

--- src/main/java/com/bk/gfg/ds/linked/list/circular/CircularLinkedList.java
+++ src/main/java/com/bk/gfg/ds/linked/list/circular/CircularLinkedList.java
@@ -1,9 +1,13 @@
 package com.bk.gfg.ds.linked.list.circular;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class CircularLinkedList {
 
 	Node head;
+	Node head1;
+	Node head2;
 
 	protected static class Node{
 		int data;
@@ -13,32 +17,60 @@ public class CircularLinkedList {
 		}
 	}
 
-	public void insertAtEnd(int data) {
+	public Node insertAtEnd(Node node ,int data,Node head) {
 		Node newNode = new Node(data);
 		if(head==null) {
-			head = newNode;
+			node = newNode;
 			newNode.next=head;
-			return;
+			return node;
 		}
-		Node node =head;
-		while(node.next!=null && node.next!=head) {
-			node=node.next;
+		Node temp =head;
+		while(temp.next!=head) {
+			temp=temp.next;
 		}
-		node.next=newNode;
+		temp.next=newNode;
 		newNode.next=head;
+		return temp;
 	}
-	
+
 	public void insertAtFront(int data) {
 		Node node = new Node(data);
 		node.next=head;
-		head =node;
-		Node temp =head;
-		if(temp.next!=null) {
-			while(temp.next!=null) {
+		if(head==null) {
+			head=node;
+			node.next=head;
+		}else {
+			Node temp =head;
+			while(temp.next!=head){
 				temp=temp.next;
 			}
+			head =node;
 			temp.next=head;
 		}
+	}
+	
+	public List<Node> splitList() {
+		List<Node> list= new ArrayList<>();
+		Node result1 =null;
+		Node result2 =null;
+		Node slow = head;
+		Node fast = head;
+		while(slow.next!=head && fast.next.next!=head) {
+			result1=insertAtEnd(result1,slow.data,head1);
+			slow=slow.next;
+			fast=fast.next.next;
+		}
+		slow=head;
+		fast=head;
+		while(slow.next!=head && fast.next.next!=head) {
+			result2=insertAtEnd(result2,fast.data,head2);
+			slow=slow.next;
+			fast=fast.next.next;
+		}
+		
+		list.add(result1);
+		list.add(result2);
+		return list;
 	}
 
 	public void printList() {
@@ -50,15 +82,34 @@ public class CircularLinkedList {
 			}while(node!=head);
 		}
 	}
+	
+	public void printSplitList(List<Node> nodeList) {
+		Node node1=nodeList.get(0);
+		Node node2=nodeList.get(1);
+		if(node1!=null) {
+			do {
+				System.out.println(node1.data);
+				node1=node1.next;
+			}while(node1!=head);
+		}
+		System.out.println("---------------------");
+		if(node2!=null) {
+			do {
+				System.out.println(node2.data);
+				node2=node2.next;
+			}while(node2!=head);
+		}
+	}
 
 	public static void main(String[] args) {
 		CircularLinkedList list = new CircularLinkedList();
 		list.insertAtFront(1);
-		list.printList();
 		list.insertAtFront(2);
 		list.insertAtFront(3);
 		list.insertAtFront(4);
 		list.printList();
+		System.out.println("#########");
+		list.printSplitList(list.splitList());
 	}
 
 }
