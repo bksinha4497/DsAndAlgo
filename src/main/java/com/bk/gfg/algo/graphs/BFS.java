11f6e20d92fab8bb80c60e1fb9e5aca87877b8ba
biswajit.kumar1 <biswajit.kumar1@comviva.com>
2022-08-07 18:34:29 +0530

    Algos - Graphs

--- src/main/java/com/bk/gfg/algo/graphs/BFS.java
+++ src/main/java/com/bk/gfg/algo/graphs/BFS.java
@@ -25,37 +25,35 @@ public class BFS {
     }
  
     // prints BFS traversal from a given source s
-    void BFSTraversal(int s)
-    {
-    	 // Create a queue for BFS
-        LinkedList<Integer> queue = new LinkedList<Integer>();
-        // Mark all the vertices as not visited(By default
-        // set as false)
-        boolean visited[] = new boolean[V];
-        // Mark the current node as visited and enqueue it
-        visited[s]=true;
-        queue.add(s);
-        
-        while (queue.size() != 0)
-        {
-            // Dequeue a vertex from queue and print it
-            s = queue.poll();
-            System.out.print(s+" ");
- 
-            // Get all adjacent vertices of the dequeued vertex s
-            // If a adjacent has not been visited, then mark it
-            // visited and enqueue it
-            Iterator<Integer> i = adj.get(s).listIterator();
-            while (i.hasNext())
-            {
-                int n = i.next();
-                if (!visited[n])
-                {
-                    visited[n] = true;
-                    queue.add(n);
-                }
-            }
-        }
+    void BFSTraversal(int s){
+    	// Create a queue for BFS
+    	LinkedList<Integer> queue = new LinkedList<Integer>();
+    	// Mark all the vertices as not visited(By default
+    	// set as false)
+    	boolean visited[] = new boolean[V];
+    	// Mark the current node as visited and enqueue it
+    	visited[s]=true;
+    	queue.add(s);
+
+    	while (queue.size() != 0)
+    	{
+    		// Dequeue a vertex from queue and print it
+    		s = queue.poll();
+    		System.out.print(s+" ");
+    		// Get all adjacent vertices of the dequeued vertex s
+    		// If a adjacent has not been visited, then mark it
+    		// visited and enqueue it
+    		Iterator<Integer> i = adj.get(s).listIterator();
+    		while (i.hasNext())
+    		{
+    			int n = i.next();
+    			if (!visited[n])
+    			{
+    				visited[n] = true;
+    				queue.add(n);
+    			}
+    		}
+    	}
     }
  
     // Driver method to
