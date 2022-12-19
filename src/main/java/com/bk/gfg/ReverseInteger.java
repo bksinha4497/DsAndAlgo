6fca3ce46167ac0e3cae8c1126610f7d41bd09fe
biswajit.kumar1 <biswajit.kumar1@comviva.com>
2022-12-19 16:33:54 +0530

    commiting changes for reverse interger

--- src/main/java/com/bk/gfg/ReverseInteger.java
+++ src/main/java/com/bk/gfg/ReverseInteger.java
@@ -6,34 +6,23 @@ public class ReverseInteger {
     public static void main(String[] args) {
 	ReverseInteger obj = new ReverseInteger();
 	int result = obj.reverse(123);
-	System.out.println("result :: "+result);
+	System.out.println("result :: " + result);
 	result = obj.reverse(120);
-	System.out.println("result :: "+result);
+	System.out.println("result :: " + result);
 	result = obj.reverse(-1205);
-	System.out.println("result :: "+result);
+	System.out.println("result :: " + result);
+	result = obj.reverse(1534236469);
+	System.out.println("result :: " + result);
     }
+
     public int reverse(int x) {
-	StringBuilder sb = new StringBuilder();
-	String str = String.valueOf(x);
-	boolean isPlus=false;
-	boolean isMinus=false;
-	for(int i=str.length()-1;i>=0;i--){
-	    if(str.charAt(i)=='-')
-		isMinus=true;
-	    if(str.charAt(i)=='+')
-		isPlus=true;
-	    if((str.charAt(i)=='+' || str.charAt(i)=='-') && (i==0  || i==str.length()-1 ))
-		continue;
-	    if(str.charAt(i)!='-' && str.charAt(i)!='+'){
-		sb.append(str.charAt(i));
-	    }
+	int res = 0;
+	while (x != 0) {
+	    if (Math.abs(res) > Integer.MAX_VALUE / 10)
+		return 0;
+	    res = res * 10 + x % 10;
+	    x = x / 10;
 	}
-	String result=sb.toString();
-	if(isPlus){
-	    result="+"+result;
-	}else if(isMinus){
-	    result="-"+result;
-	}
-	return Integer.valueOf(result);
+	return res;
     }
 }
